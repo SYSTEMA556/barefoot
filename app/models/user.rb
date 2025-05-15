817bd68c360abb8b15b9e3ab9d5903b759cecc9d
SYSTEMA556 <systema616@gmail.com>
2025-05-15 21:22:34 +0900

    dep

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,9 @@ class User < ApplicationRecord
    # def send_confirmation_email
    #  UserMailer.email_confirmation(self).deliver_later
  #  end
+
+   def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
