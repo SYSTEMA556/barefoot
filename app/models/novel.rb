2fcfe49e01893a0757840fefa4fc6a5682a8af61
SYSTEMA556 <systema616@gmail.com>
2025-05-17 22:21:39 +0900

    message

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,5 +1,7 @@
 class Novel < ApplicationRecord
   belongs_to :user, optional: true
    enum status: { draft: 0, published: 1 }
-
+  validates :title, presence: true
+  validates :author_name, presence: true
+  validates :body, presence: true
 end
