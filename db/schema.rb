2af4262aa33156295d64fa1482a052eb70eb06f5
SYSTEMA556 <systema616@gmail.com>
2025-05-19 21:32:30 +0900

    ?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_18_150822) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_18_152158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_18_150822) do
     t.datetime "confirmed_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "activation_digest"
+    t.boolean "activated"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
