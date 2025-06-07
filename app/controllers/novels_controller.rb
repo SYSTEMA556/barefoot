17cc02e9d4c7dab5ffe62b490e93d6762d372114
SYSTEMA556 <systema616@gmail.com>
2025-06-07 22:27:53 +0900

    sa

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,6 +1,7 @@
 class NovelsController < ApplicationController
-  before_action :set_novel, only: [:show, :enter_password, :verify_password, :edit, :update, :destroy]
-  before_action :set_search, only: [:index, :show, :new, :edit, :create, :update, :destroy]
+  before_action :set_novel, only: [:show, :enter_password, :edit, :update, :destroy]
+  before_action :set_search, only: [:index, :show, :new, :edit, :create, :update, :destroy, :preview]
+  skip_before_action :verify_authenticity_token, only: [:preview]
 
   def index
     # Ransack で検索オブジェクトを生成
@@ -23,6 +24,11 @@ class NovelsController < ApplicationController
 
     # ページネーション（will_paginate を想定）
     @novels = @novels.paginate(page: params[:page], per_page: 10)
+  end
+  def preview
+    @novel          = Novel.new(novel_params)
+    @preview_params = novel_params
+    render :preview
   end
 
   def show
