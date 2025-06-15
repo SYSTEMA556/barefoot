8220d382a9310d233683e4000c0e9ebcda30ad1a
SYSTEMA556 <systema616@gmail.com>
2025-06-15 19:14:07 +0900

    neko

--- db/migrate/20250615100629_create_structure_from_schema.rb
+++ db/migrate/20250615100629_create_structure_from_schema.rb
@@ -1,18 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.1].define(version: 2025_06_15_100629) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+class CreateStructureFromSchema < ActiveRecord::Migration[7.1]
+  def change
+    enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -162,4 +150,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_15_100629) do
   add_foreign_key "novel_tags", "tags"
   add_foreign_key "novels", "users"
   add_foreign_key "taggings", "tags"
-end
+    end
+  end
