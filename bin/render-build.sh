ebf4785f9193e6d1efd79c5f2b9d5859d584bfdd
SYSTEMA556 <systema616@gmail.com>
2025-06-15 17:29:23 +0900

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
+set -o xtrace   # こちらを足してデバッグ出力を有効化！
 
 bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
 
-bundle exec rails db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1   # 初回用
-bundle exec rails db:migrate                                            # 継続用
+bundle exec rails db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1 || true
+bundle exec rails db:migrate
